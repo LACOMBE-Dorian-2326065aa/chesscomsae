fd748ec4f293c6fd267d71619c003f0b75a0d1f6
VALENTE-Hugo-2326110aa <hugo.valente@etu.univ-amu.fr>
2024-06-03 11:19:10 +0200

    Ajout du Pion

---
+++ src/main/java/fr/iut/chesscomsae/piece/Pion.java
@@ -0,0 +1,17 @@
+package fr.iut.chesscomsae.piece;
+
+public class Pion extends Piece {
+
+    public Pion(int x, int y, boolean isWhite) {
+        super(x, y, isWhite);
+    }
+
+    @Override
+    public boolean isMoveLegal(int x, int y) {
+        if (isWhite()) {
+            return y == getRow() + 1 && x == getColumn();
+        } else {
+            return y == getRow() - 1 && x == getColumn();
+        }
+    }
+}
