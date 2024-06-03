818c5393b8c12fdab2b67918ffde126355744310
LACOMBE Dorian <dorian.lacombe@etu.univ-amu.fr>
2024-06-03 15:23:03 +0200

    Le bouton jouer propose maintenant aux 2 joueur d'entrer leur prénom et leur nom pour créer les 2 instances de Joueur correspondantes

--- src/main/java/fr/iut/chesscomsae/ChessController.java
+++ src/main/java/fr/iut/chesscomsae/ChessController.java
@@ -5,10 +5,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -18,8 +20,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ChessController implements Initializable {
 
-    @FXML
-    private ImageView userImageView;
+    /**
+     * Initialisation de tous les éléments nécessaires implémentés en FXML
+     */
     @FXML
     private GridPane chessBoard;
     @FXML
@@ -34,7 +37,24 @@ public class ChessController implements Initializable {
     private Label timerEnnemy;
     @FXML
     private Button buttonPlay;
+    @FXML
+    private VBox newButtons;
 
+    private Label prenomLabel;
+    private TextField prenom;
+    private Label nomLabel;
+    private TextField nom;
+    private Button valid;
+
+    private Joueur j1;
+    private Joueur j2;
+
+    /**
+     * Initialise les données de la fenêtre
+     * @author Dorian Lacombe
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         createChessBoard();
@@ -43,6 +63,10 @@ public class ChessController implements Initializable {
         labelGames.setText(Integer.toString(ThreadLocalRandom.current().nextInt(13000000, 14000001)));
     }
 
+    /**
+     * Crée les 8x8 cases du plateau en mettant automatiquement les couleurs
+     * @author Dorian Lacombe
+     */
     public void createChessBoard() {
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
@@ -62,11 +86,48 @@ public class ChessController implements Initializable {
         }
     }
 
+    /**
+     * Fonction exécutée lorsqu'on clique sur le bouton JOUER qui démarre la partie et initialise les timer
+     * @author Dorian Lacombe
+     */
     public void play() {
         String timerString = choiceBox.getSelectionModel().getSelectedItem();
         timerString = timerString.replace(" min", ":00").replace("5", "05");
         timerMe.setText(timerString);
         timerEnnemy.setText(timerString);
         buttonPlay.setDisable(true);
+
+        prenomLabel = new Label("Prénom J1 :");
+        prenom = new TextField();
+        nomLabel = new Label("Nom J1 :");
+        nom = new TextField();
+        valid = new Button("Valider");
+
+        prenomLabel.getStyleClass().add("prenomLabel");
+        prenom.getStyleClass().add("prenom");
+        nomLabel.getStyleClass().add("nomLabel");
+        nom.getStyleClass().add("nom");
+        valid.getStyleClass().add("valid");
+
+        newButtons.getChildren().addAll(prenomLabel, prenom, nomLabel, nom, valid);
+
+        valid.onActionProperty().set(actionEvent -> validation(true));
+    }
+
+    public void validation(boolean isJ1) {
+        if(isJ1) {
+            j1 = new Joueur(nom.getText(), prenom.getText(), true);
+            prenomLabel.setText("Prénom J2 :");
+            nomLabel.setText("Nom J2 :");
+            nom.setText("");
+            prenom.setText("");
+            valid.onActionProperty().set(actionEvent -> validation(false));
+        } else {
+            j2 = new Joueur(nom.getText(), prenom.getText(), false);
+            newButtons.getChildren().removeAll(prenom, prenomLabel, nom, nomLabel, valid);
+            System.out.println(j1);
+            System.out.println(j2);
+        }
+
     }
 }
