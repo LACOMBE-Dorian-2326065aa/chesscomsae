0a8b669c00669db443b2b5cfbbe46f8d2388f104
LACOMBE-Dorian-2326065aa <dorian.lacombe@etu.univ-amu.fr>
2024-06-03 21:51:24 +0200

    Dernier réglages dans l'organisation des fichiers resources et adaptation du code en conséquence

--- src/main/java/fr/iut/chesscomsae/Chess.java
+++ src/main/java/fr/iut/chesscomsae/Chess.java
@@ -3,18 +3,22 @@ package fr.iut.chesscomsae;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class Chess extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Chess.class.getResource("chess.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
-        stage.setTitle("Chess.com");
-        String css = this.getClass().getResource("style.css").toExternalForm();
+        HBox root = FXMLLoader.load(getClass().getClassLoader().getResource("chess.fxml"));
+        Scene scene = new Scene(root, 1080, 720);
+
+        String css = getClass().getClassLoader().getResource("style.css").toExternalForm();
         scene.getStylesheets().add(css);
+
+        stage.setTitle("Chess.com");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
