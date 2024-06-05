a7f127afdb622a9a3bfad70afb3d561a083130de
VALENTE-Hugo-2326110aa <hugo.valente@etu.univ-amu.fr>
2024-06-05 23:14:33 +0200

    - Ajout de commentaire à la classe Joueur - Ajout d'un setter pour nombrePartiesGagnees et nombrePartiesJouees - Suppression de method/fonction super flux

--- src/main/java/fr/iut/chesscomsae/Joueur.java
+++ src/main/java/fr/iut/chesscomsae/Joueur.java
@@ -1,13 +1,14 @@
 package fr.iut.chesscomsae;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 
 import java.io.*;
 
 public class Joueur {
 
+    /**
+     * Attributs de la classe Joueur
+     */
     @Expose
     public String nom;
     @Expose
@@ -21,6 +22,10 @@ public class Joueur {
 
 
 
+    /**
+     * Constructeur de la classe Joueur
+     * @author Turmo Baptiste
+     */
     public Joueur(String nom, String prenom, boolean estBlanc) {
         this.nom = nom;
         this.prenom = prenom;
@@ -29,71 +34,99 @@ public class Joueur {
         this.estBlanc = estBlanc;
     }
 
+    /**
+     * Constructeur de la classe Joueur
+     * @author Valente Hugo
+     */
     public Joueur(String nom, String prenom) {
         this(nom, prenom, false);
     }
 
+    /**
+     * Permets de changer le nombre de parties gagnées
+     * @author Valente Hugo
+     */
+    public void setNombrePartiesGagnees(int nombrePartiesGagnees) {
+        this.nombrePartiesGagnees = nombrePartiesGagnees;
+    }
 
+    /**
+     * Permets de changer le nombre de parties jouées
+     * @author Valente Hugo
+     * @param nombrePartiesJouees le nombre de parties jouées
+     */
+    public void setNombrePartiesJouees(int nombrePartiesJouees) {
+        this.nombrePartiesJouees = nombrePartiesJouees;
+    }
 
-
-    // Ici on a les getters et les setters pour les attributs de la classe Joueur
+    /**
+     * Permets de récupérer le nom du joueur
+     * @author Turmo Baptiste
+     * @return le nombre de parties jouées
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Permets de changer le nom du joueur
+     * @author Turmo Baptiste
+     * @param nom le nom du joueur
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Permets de récupérer le prénom du joueur
+     * @author Turmo Baptiste
+     * @return le prénom du joueur
+     */
     public String getPrenom() {
         return prenom;
     }
 
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
+    /**
+     * Permet d'obtenir le nombre de parties jouées par un joueur
+     * @author Turmo Baptiste
+     * @return le nombre de parties jouées
+     */
     public int getNombrePartiesJouees() {
         return nombrePartiesJouees;
     }
 
-    public void incrementerNombrePartiesJouees() {
-        this.nombrePartiesJouees++;
-    }
-
+    /**
+     * Permet d'obtenir le nombre de parties gagnées par un joueur
+     * @author Turmo Baptiste
+     * @return le nombre de parties gagnées
+     */
     public int getNombrePartiesGagnees() {
         return nombrePartiesGagnees;
     }
 
-    public void incrementerNombrePartiesGagnees() {
-        this.nombrePartiesGagnees++;
-    }
-
-    // Getter pour la couleur
+    /**
+     * Permet de savoir si le joueur est blanc ou non
+     * @author Turmo Baptiste
+     * @return true si le joueur est blanc, false sinon
+     */
     public boolean estBlanc() {
         return estBlanc;
     }
 
-    // Setter pour la couleur
+    /**
+     * Permet de changer la couleur du joueur
+     * @author Valente Hugo
+     * @param estBlanc true si le joueur est blanc, false sinon
+     */
     public void setEstBlanc(boolean estBlanc) {
         this.estBlanc = estBlanc;
     }
 
-    // Ici on enregistre un joueur dans un fichier
-    public void sauvegarderJoueur(String cheminFichier) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(cheminFichier))) {
-            oos.writeObject(this);
-        }
-    }
-
-
-    // Ici on lit un joueur depuis un fichier
-    public static Joueur lireJoueur(String cheminFichier) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(cheminFichier))) {
-            return (Joueur) ois.readObject();
-        }
-    }
-
+    /**
+     * Permet d'avoir une représentation textuelle d'un joueur
+     * @author Turmo Baptiste
+     * @return une chaîne de caractères représentant un joueur
+     */
     @Override
     public String toString() {
         return "Joueur[" +
@@ -103,10 +136,6 @@ public class Joueur {
                 ", nombrePartiesGagnees=" + nombrePartiesGagnees +
                 ", estBlanc=" + (estBlanc ? "true" : "false") +
                 ']';
-    }
-
-    public String joueurToJson() {;
-        return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create().toJson(this);
     }
 
 }
